e0d24e656e1ba54445ad1945316680d3dd14fa98
Lars Straathof <lars.straathof@ing.com>
2021-04-23 17:30:00 +0200

    chore: changed config for github pages

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,17 @@
+const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'source-map'
+  devtool: 'source-map',
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        // { from: "./example/**.*", to: "" },
+        path.resolve(__dirname, 'example'),
+      ],
+    }),
+  ],
 });
